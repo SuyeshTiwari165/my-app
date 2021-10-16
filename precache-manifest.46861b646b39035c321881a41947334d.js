31cdb563b4776294c1689f258604de1ba2871963
Suyesh Tiwari <suyeshtiwari123@gmail.com>
2021-10-16 18:43:34 +0530

    Updates

---
+++ precache-manifest.46861b646b39035c321881a41947334d.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdfeedaf72e6ed123306187f8814a940",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "d0d7643036e06114103e",
+    "url": "/my-app/static/css/main.4c39b24e.chunk.css"
+  },
+  {
+    "revision": "db0611e9b74474a92ffb",
+    "url": "/my-app/static/js/2.ca8ba157.chunk.js"
+  },
+  {
+    "revision": "8467ea10efaffed4f27959eb4dce40d4",
+    "url": "/my-app/static/js/2.ca8ba157.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acc88cd615b35c96d619",
+    "url": "/my-app/static/js/3.821e9e9d.chunk.js"
+  },
+  {
+    "revision": "783f14fa45b10e088e68f98251448010",
+    "url": "/my-app/static/js/3.821e9e9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3273440d88048a39a654",
+    "url": "/my-app/static/js/4.d87ade33.chunk.js"
+  },
+  {
+    "revision": "aa0d01a96b90795adfcac80081b04497",
+    "url": "/my-app/static/js/4.d87ade33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62594a489c09fb867be6",
+    "url": "/my-app/static/js/5.21661c87.chunk.js"
+  },
+  {
+    "revision": "54a13f759b1799645c0f4c440498a75c",
+    "url": "/my-app/static/js/5.21661c87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0d7643036e06114103e",
+    "url": "/my-app/static/js/main.e91a21e1.chunk.js"
+  },
+  {
+    "revision": "19a4ce96c27aed3a46d6",
+    "url": "/my-app/static/js/runtime-main.bc7527ba.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/my-app/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "77393dad5972e944a9c882bdcec7866a",
+    "url": "/my-app/static/media/dashboardImg1.77393dad.jpg"
+  },
+  {
+    "revision": "221d8c0bb9a8c0295db45d9bbf353afc",
+    "url": "/my-app/static/media/dashboardImg2.221d8c0b.jpg"
+  },
+  {
+    "revision": "a25da86846be0431852e1dcdf6a810b8",
+    "url": "/my-app/static/media/dashboardImg3.a25da868.png"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/my-app/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/my-app/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "66f37ba5c7e4b6648f04883fa637e634",
+    "url": "/my-app/static/media/feature-tile-icon-03.66f37ba5.svg"
+  },
+  {
+    "revision": "836acd100a942420f8ab4e6c5e4162d8",
+    "url": "/my-app/static/media/feature-tile-icon-04.836acd10.svg"
+  },
+  {
+    "revision": "fa9ba00b178abc7bd2e02bd986e91fe3",
+    "url": "/my-app/static/media/feature-tile-icon-05.fa9ba00b.svg"
+  },
+  {
+    "revision": "6a1776963deb39d710bae80bb449aebe",
+    "url": "/my-app/static/media/feature-tile-icon-06.6a177696.svg"
+  },
+  {
+    "revision": "475ac253f34a2bad87816e3ed4e06bc4",
+    "url": "/my-app/static/media/hemplogo.475ac253.png"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/my-app/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/my-app/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "27ca52808962249b84a90b4da2dc833f",
+    "url": "/my-app/static/media/img1.27ca5280.webp"
+  }
+]);
